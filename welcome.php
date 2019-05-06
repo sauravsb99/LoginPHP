8c656aee1596ccd1c4bb1ae4156688f5cb56b683
Saurav S B <32162836+sauravsb99@users.noreply.github.com>
2019-05-06 20:47:29 +0530

    Puthiya item
    idea ond pakshe pattande

--- welcome.php
+++ welcome.php
@@ -13,6 +13,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <title>Welcome</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
@@ -23,9 +27,52 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <div class="page-header">
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
     </div>
-    <p>
+
+
+    <div class="container">
+  <h2>Collapsible Panel</h2>
+  <p>Click on the collapsible panel to open and close it.</p>
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" href="#collapse1">Saurav</a>
+        </h4>
+      </div>
+      <div id="collapse1" class="panel-collapse collapse">
+        <div class="panel-body">9.02</div>
+        <div class="panel-body">8.40</div>
+        <div class="panel-footer">8.76</div>
+         </div>
+         
+        <div class="panel-heading"> 
+         <h4 class="panel-title">
+          <a data-toggle="collapse" href="#collapse2">Suman</a>
+        </h4>
+     	</div>
+      <div id="collapse2" class="panel-collapse collapse">
+        <div class="panel-body">9.02</div>
+        <div class="panel-body">8.40</div>
+        <div class="panel-footer">8.76</div>
+         
+         </div>
+         
+         
+         
+         
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+    <!-- <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
-    </p>
+    </p> -->
 </body>
 </html>
